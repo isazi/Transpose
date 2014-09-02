99c1b150078643b033d75022ba30b0a8606a5e29
Alessio Sclocco <alessio@sclocco.eu>
2014-09-02 15:19:24 +0200

    Adding an AVX transpose.

--- include/Transpose.hpp
+++ include/Transpose.hpp
@@ -15,7 +15,6 @@
 #include <vector>
 #include <string>
 
-#include <Exceptions.hpp>
 #include <utils.hpp>
 
 
@@ -29,6 +28,8 @@ namespace OpenCL {
 template< typename T > void transpose(const unsigned int M, const unsigned int N, const unsigned int padding, std::vector< T > &input, std::vector< T > &output);
 // OpenCL transpose
 std::string * getTransposeOpenCL(const unsigned int nrThreads, const unsigned int M, const unsigned int N, const unsigned int padding, const unsigned int vector, std::string typeName);
+// AVX transpose
+void transposeAVX(const unsigned int M, const unsigned int N, const float * input, float * output);
 
 
 // Implementations
@@ -85,6 +86,54 @@ std::string * getTransposeOpenCL(const unsigned int nrThreads, const unsigned in
   // End kernel's template
 
   return code;
+}
+
+void transposeAVX(const unsigned int M, const unsigned int N, const float * input, float * output) {
+  #pragma omp parallel for schedule(static)
+  for ( unsigned int m = 0; m < M; m += 8 ) {
+    #pragma omp parallel for schedule(static)
+    for ( unsigned int n = 0; n < N; n += 8 ) {
+      __m256 buffer[8];
+
+      // Load input
+      for ( unsigned int i = 0; i < 8; i++ ) {
+        buffer[i] =  _mm256_load_ps(&(input[((m + i) * isa::utils::pad(N, 8)) + n])); 
+      }
+
+      // Local in-place AVX transpose: https://stackoverflow.com/questions/16941098/fast-memory-transpose-with-sse-avx-and-openmp
+      __m256 __t0, __t1, __t2, __t3, __t4, __t5, __t6, __t7;
+      __m256 __tt0, __tt1, __tt2, __tt3, __tt4, __tt5, __tt6, __tt7;
+      __t0 = _mm256_unpacklo_ps(buffer[0], buffer[1]);
+      __t1 = _mm256_unpackhi_ps(buffer[0], buffer[1]);
+      __t2 = _mm256_unpacklo_ps(buffer[2], buffer[3]);
+      __t3 = _mm256_unpackhi_ps(buffer[2], buffer[3]);
+      __t4 = _mm256_unpacklo_ps(buffer[4], buffer[5]);
+      __t5 = _mm256_unpackhi_ps(buffer[4], buffer[5]);
+      __t6 = _mm256_unpacklo_ps(buffer[6], buffer[7]);
+      __t7 = _mm256_unpackhi_ps(buffer[6], buffer[7]);
+      __tt0 = _mm256_shuffle_ps(__t0,__t2,_MM_SHUFFLE(1,0,1,0));
+      __tt1 = _mm256_shuffle_ps(__t0,__t2,_MM_SHUFFLE(3,2,3,2));
+      __tt2 = _mm256_shuffle_ps(__t1,__t3,_MM_SHUFFLE(1,0,1,0));
+      __tt3 = _mm256_shuffle_ps(__t1,__t3,_MM_SHUFFLE(3,2,3,2));
+      __tt4 = _mm256_shuffle_ps(__t4,__t6,_MM_SHUFFLE(1,0,1,0));
+      __tt5 = _mm256_shuffle_ps(__t4,__t6,_MM_SHUFFLE(3,2,3,2));
+      __tt6 = _mm256_shuffle_ps(__t5,__t7,_MM_SHUFFLE(1,0,1,0));
+      __tt7 = _mm256_shuffle_ps(__t5,__t7,_MM_SHUFFLE(3,2,3,2));
+      buffer[0] = _mm256_permute2f128_ps(__tt0, __tt4, 0x20);
+      buffer[1] = _mm256_permute2f128_ps(__tt1, __tt5, 0x20);
+      buffer[2] = _mm256_permute2f128_ps(__tt2, __tt6, 0x20);
+      buffer[3] = _mm256_permute2f128_ps(__tt3, __tt7, 0x20);
+      buffer[4] = _mm256_permute2f128_ps(__tt0, __tt4, 0x31);
+      buffer[5] = _mm256_permute2f128_ps(__tt1, __tt5, 0x31);
+      buffer[6] = _mm256_permute2f128_ps(__tt2, __tt6, 0x31);
+      buffer[7] = _mm256_permute2f128_ps(__tt3, __tt7, 0x31);
+
+      // Store output
+      for ( unsigned int i = 0; i < 8; i++ ) {
+        _mm256_store_ps(&(input[((n + i) * isa::utils::pad(M, 8)) + m]), buffer[i]); 
+      }
+    }
+  }
 }
 
 } // OpenCl
